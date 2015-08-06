d5eadde0154790a6e8834c1db6c3295784d5f66d
sharkbound <laptopblaster@gmail.com>
2015-08-06 13:24:14 -0500

    fixed controls and made projectiles have forward momentum and disapear after a delay and made level better

---
+++ First3Dproject/Assets/DestroyFireball.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyFireball : MonoBehaviour {
+	public float DestroyDelay;
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, DestroyDelay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- First3Dproject/Assets/moveScript.cs
+++ First3Dproject/Assets/moveScript.cs
@@ -8,7 +8,9 @@ public class moveScript : MonoBehaviour {
 	public float JumpSpeed = 6.0f;
 	public float Gravity = 20.0f;
 	public float RotateSpeed = 6.0f;
-	public Transform BulletPrefab;
+	public float BulletSpeed;
+	public Rigidbody BulletPrefab;
+	public Transform ShootLocation;
 	private Vector3 moveDirection = Vector3.zero;
 	// Use this for initialization
 	void Start () {
@@ -23,34 +25,37 @@ public class moveScript : MonoBehaviour {
 		//is the player on ground?
 		if (controller.isGrounded) {
 
-			//feed move direction with input
-			//moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
-
 			//rotate camera
-			transform.Rotate(0, Input.GetAxis("Horizontal") * RotateSpeed, 0);
+			transform.Rotate(0, -Input.GetAxis("Horizontal") * RotateSpeed, 0);
 
-			//multiply it by speed
-			moveDirection *= Speed;
-
-			//Jumping
+			//shooting stuff once per key press
 			if (Input.GetButtonDown("Jump"))
 			{
-				//moveDirection.y = JumpSpeed;
-				 Object bullet = Instantiate(BulletPrefab,
-				                   GameObject.Find("spawnPoint").transform.position,
-				                    Quaternion.identity);
-
-			}
+				Rigidbody bullet = Instantiate(BulletPrefab,
+				                                  GameObject.Find("spawnPoint").transform.position,
+				                                  Quaternion.identity) as Rigidbody;
+				bullet.AddForce(ShootLocation.forward * BulletSpeed);
 			
+			}
+			if (Input.GetKey(KeyCode.M))
+			{
+				Rigidbody bullet = Instantiate(BulletPrefab,
+				                               GameObject.Find("spawnPoint").transform.position,
+				                               Quaternion.identity) as Rigidbody;
+				bullet.AddForce(ShootLocation.forward * BulletSpeed);
+			}
+
+
+			//feed move direction with input
+			moveDirection = new Vector3(0, 0, -Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			//multiply it by speed
+			moveDirection *= Speed;
 		}
-		
 		//Applying gravity to the controller
 		moveDirection.y -= Gravity * Time.deltaTime;
 
 		//Making the character move
-		controller.Move(moveDirection * Time.deltaTime);
-
+		 controller.Move(moveDirection * Time.deltaTime);
 	}
 }
