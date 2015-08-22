ff4b6c796719e4f7cf4e669d2028dcf0e58c4c3b
sharkbound <laptopblaster@gmail.com>
2015-08-22 16:44:46 -0500

    gun now shoots a "laser" and can  dmg objects with the dmg script on them

---
+++ lasertag/Assets/Scripts/HitLifeReduce.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitLifeReduce : MonoBehaviour {
+
+	public int MaxLife = 100;
+	public int CurrentLife = 100;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void damage(int dmg){
+
+		CurrentLife -= dmg;
+		Debug.Log (CurrentLife);
+		if (CurrentLife <= 0){
+			//Destroy(transform.gameObject);
+
+			transform.gameObject.SetActive(false);
+		}
+
+	}
+}
--- lasertag/Assets/Scripts/raycast.cs
+++ lasertag/Assets/Scripts/raycast.cs
@@ -5,7 +5,12 @@ public class raycast : MonoBehaviour {
 
 	public Camera cam;
 	public Transform gun;
+	public int AutoDmg = 5;
+	public int dmg = 20;
+	public int DmgDelay = 60;
+	private int timePassed = 0;
 	private LineRenderer line;
+	private bool DoAuto = false;
 	// Use this for initialization
 	void Start () {
 		line = GameObject.Find("laserspawn").gameObject.GetComponent<LineRenderer>();
@@ -14,10 +19,32 @@ public class raycast : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-			fireLaserOnce ();
+		ToggleAuto ();
+		fireSelection ();
+		if (Input.GetKeyDown(KeyCode.R)){
+		//	GameObject.Find("enemy").SetActive(true);
+
+		}
 	}
 	void DisableLine(){
 		line.enabled = false;
+	}
+	void ToggleAuto(){
+		if (Input.GetKeyDown(KeyCode.M)){
+			Debug.Log("toggled! " + DoAuto);
+			DoAuto = !DoAuto;
+		}
+	}
+	void fireSelection(){
+		if (Input.GetButton ("Fire1")) {
+			if (DoAuto) {
+				fireLaserRapid ();
+			} else if (!DoAuto) {
+				fireLaserOnce ();
+			}
+		} else {
+			line.enabled = false;
+		}
 	}
 	void fireLaserOnce()
 	{
@@ -29,14 +56,37 @@ public class raycast : MonoBehaviour {
 			if (Physics.Raycast (ray, out hit, 100)) {
 				line.enabled = true;
 				line.SetPosition (1, hit.point);
-				//Invoke("DisableLine", 2);
+				Invoke("DisableLine", 0.1f);
+				hit.transform.SendMessage("damage", dmg, SendMessageOptions.DontRequireReceiver);
+				//Destroy(hit.transform.gameObject);
 			} else {
 				line.enabled = true;
 				line.SetPosition (1, ray.GetPoint (60));
-				//Invoke("DisableLine", 2);
+				Invoke("DisableLine", 0.1f);
+			}
+		}
+	}
+	void fireLaserRapid()
+	{
+		timePassed++;
+
+		line.enabled = true;
+		if (Input.GetButton ("Fire1")) {
+			Ray ray = new Ray (cam.transform.position, cam.transform.forward);
+			RaycastHit hit;
+			line.SetPosition (0, transform.Find ("laserspawn").position);
+			
+			if (Physics.Raycast (ray, out hit, 100)) {
+				line.SetPosition (1, hit.point);
+				if (timePassed >= DmgDelay){
+					hit.transform.SendMessage("damage", AutoDmg, SendMessageOptions.DontRequireReceiver);
+					timePassed = 0;
+				}
+			} else {
+				line.SetPosition (1, ray.GetPoint (60));
 			}
 		} else {
-			line.enabled = false;
+			line.enabled = false; 
 		}
 	}
 }
